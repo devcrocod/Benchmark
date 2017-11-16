a847fbad1c6cbc35c6fb5d05a69dc9e8b171a324
devcrocod <devcrocod@gmail.com>
2017-11-16 23:39:21 +0300

    change main

--- pgsql-rds/main.tf
+++ pgsql-rds/main.tf
@@ -1,17 +1,18 @@
-# provider "aws" {
-#	access_key = "${var.aws_access_key}"
-#	secret_key = "${var.aws_secret_key}"
-#	region     = "eu-west-1"
-#}
+provider "aws" {
+	access_key = "${var.aws_access_key}"
+	secret_key = "${var.aws_secret_key}"
+	region = "eu-west-1"
+}
 
 resource "aws_db_instance" "default" {
-	allocated_storage 	 = 1
-	identifier			 = "dbpg-rds"
-	storage_type		 = "gp2"
-	engine 			  	 = "postgres"
-	engine_version 	  	 = "10.0"
-	instance_class 	  	 = "db.t2.micro"
-	name 			  	 = "dbpg"
-	username 		  	 = "postgres"
-	password 		  	 = "password"
+	allocated_storage = "${var.storage}"
+	identifier = "${var.identifier}"
+	storage_type = "${var.storage_type}"
+	engine = "${var.engine}"
+	engine_version = "${var.engine_version}"
+	instance_class = "${var.instance_class}"
+	publicly_accessible = "${var.publicly_accessible}"
+	name = "${var.db_name}"
+	username = "${var.username}"
+	password = "${var.password}"
 }

005cb86904d063f5f8fcfe85b21e9e0515264223
devcrocod <devcrocod@gmail.com>
2017-11-21 19:17:28 +0300

    script for rds

---
+++ pgsql-rds/insert_test.sql
@@ -0,0 +1,1 @@
+INSERT INTO weather (city, temp_low, temp_high, precipitation, date) VALUES ('Saint-Petersburg', 10, 25, 30, '2017-07-01');
--- script.sh
+++ script.sh
@@ -2,14 +2,32 @@
 #go to the path
 path=$1
 cd $path
-# test
-pwd
 # terraform
-terraform init
-terraform plan
-# apply terraform configuration
-# terraform apply
+ans=''
+while [[ $ans != 'no' ]]; do
+	#statements
+	echo 'Do you want to create aws instance? [yes/no]'
+	read ans 
+	if [[ $ans == 'yes' ]]; then
+		terraform init
+		terraform plan
+		# apply terraform configuration
+		terraform apply
+		break
+	fi
+done
 # connect db and load schema
-# schema.sql
+ans=''
+while [[ $ans != 'no' ]]; do
+	echo 'Do you want to load schema? [yes/no]'
+	read ans
+	if [[ $ans == 'yes' ]]; then
+		host=`grep 'address' $path/terraform.tfstate | sed 's/ *"address": "//' | sed 's/",//'`
+		psql --host=$host --port=5432 --username=postgres --password --dbname=postgres -f $path/schema.sql
+	fi
+done
+
+echo "Load queries."
 # yandex.tank: ammo1 - instance, ammo2 - queries
 yandex-tank -c load.ini
+exit 0
